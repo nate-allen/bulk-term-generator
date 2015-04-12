4c93d87568199ef425c82e222b22536096db0624
Nathan Allen <email@ncallen.com>
2015-04-12 11:48:17 -0400

    Changed button class

--- views/admin/templates/settings_page_default.php
+++ views/admin/templates/settings_page_default.php
@@ -2,18 +2,34 @@
 
     <h2>Bulk Term Generator</h2>
 
+    <?php if ( !empty($error) ) : ?>
+        <div class="error">
+              <p><?= $error ?></p>
+        </div>
+    <?php endif ?>
+
     <p>First, choose a taxonomy you'd like to add terms to:</p>
 
-    <form action="tools.php" method="post">
+    <form method="post">
 
-        <label class="select" for="chosen_taxonmy">Taxonomy:</label>
-
-        <!-- Select list populated with taxonomies -->
-        <?= $taxonomy_select_list ?>
+        <table class="form-table">
+            <tbody>
+                <tr>
+                    <th scope="row">
+                        <label for="chosen_taxonmy">Taxonomy <? $hook_suffix ?></label>
+                    </th>
+                    <td>
+                        <?= $taxonomy_select_list ?>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
 
         <p class="submit">
-            <input type="submit" class="button-primary" name="btg_select_taxonomy_submit" value="Select Taxonomy">
+            <input type="submit" class="button button-secondary" name="btg_select_taxonomy_submit" value="Select Taxonomy">
         </p>
+
+        <input type="hidden" name="action" value="taxonomy_selected">
 
     </form>
 
