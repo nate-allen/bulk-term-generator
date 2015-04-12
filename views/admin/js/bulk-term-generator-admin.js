ce1501162bf787607c1a42a51dedbcc7e026f340
Nathan Allen <email@ncallen.com>
2015-04-12 11:45:32 -0400

    Added first jQuery code

--- views/admin/js/bulk-term-generator-admin.js
+++ views/admin/js/bulk-term-generator-admin.js
@@ -4,6 +4,30 @@
     // The page is ready
     $(function() {
 
+        $('#add-terms').on('click', function(e){
+            e.preventDefault();
+
+            var terms_to_add = $('#terms-to-add').val().split('\n'),
+                parent_term  = ( $('#parent_term').val() ) ? $('#parent_term option:selected').text() : false;
+
+            add_terms( terms_to_add );
+
+        });
+
+        var add_terms = function( terms ) {
+
+            var html = '<ul id="term-list">';
+
+            for (var i = 0; i < terms.length; i++) {
+                html += '<li>' + terms[i] + '</li>';
+            }
+
+            html += '</ul>';
+
+            $('#term-list-container').html(html);
+
+        };
+
     });
 
     // The window has loaded
